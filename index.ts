60fe625f553299fdf7c540de8e6dd2b4738ef2c5
Nahiyan Kamal <noyanayihan@gmail.com>
2023-03-02 10:46:11 +0100

    chore: update commander options and circle ci badge(#340)
    * chore: update commander options
    
    * chore: badge circle ci

--- index.ts
+++ index.ts
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+import { Command } from 'commander';
 import * as fs from 'fs';
 
 import printTableFromInp from './src/service';
+
+const program = new Command();
 
 program
   .option('-i, --input <value>', 'input string')
   .option('-s, --stdin', 'read input from stdin')
   .parse(process.argv);
 
-if (program.input) {
-  console.log('program.input', program.input);
-  printTableFromInp(program.input);
-} else if (program.stdin) {
+const options = program.opts();
+
+if (options.input) {
+  console.log('program.input', options.input);
+  printTableFromInp(options.input);
+} else if (options.stdin) {
   printTableFromInp(fs.readFileSync(0).toString());
 } else {
   console.log('Error: Cant detect input option');
